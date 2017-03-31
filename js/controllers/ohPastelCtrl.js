4ddaa9b5e3654eb0e683efdb88b8dbb230547bd0
fmassaretto <fabio_no_more@yahoo.com.br>
2017-03-30 23:08:46 -0300

    Revert "Fixed syntax errors"
    This reverts commit 37f17dfc9fe71661d45420571407ffbf11eac944.

--- js/controllers/ohPastelCtrl.js
+++ js/controllers/ohPastelCtrl.js
@@ -23,17 +23,10 @@
 
     $scope.pedidos = [];
 
-    var allRecheioQuantidade = [];
-
-    $scope.quantidadePasteis = function (pedido) {
-        
-    };
-
     $scope.adicionarPedido = function (pedido) {
         $scope.pedidos.push(angular.copy(pedido));
         delete $scope.pedido;
         $scope.pedidoForm.$setPristine();
-        $scope.quantidadePasteis(pedido);
     };
 
     $scope.apagarPedido = function (pedidos) {
@@ -47,7 +40,7 @@
         var custoPastel = pedido.tipoPastel.preco;
         var troco = dinherioPago - custoPastel;
 
-        if (troco < 0 || troco === isNaN || troco === null) return true;
+        if (troco < 0 || troco == NaN || troco == null) return true;
 
         pedido.troco = troco;
         return false;
