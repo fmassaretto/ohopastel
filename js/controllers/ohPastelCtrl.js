2f17c620f95acf4744ecdc95e38d9f6f74259641
fmassaretto <fabio_no_more@yahoo.com.br>
2017-04-04 14:00:12 -0300

    Method 'mostarQuantidade' in development

--- js/controllers/ohPastelCtrl.js
+++ js/controllers/ohPastelCtrl.js
@@ -18,22 +18,45 @@
       { tipo: "Hot Dog", preco: 5 },
       { tipo: "Portuguesa", preco: 5 },
       { tipo: "Brócolis c/ Queijo e Bacon", preco: 5 },
-      { tipo: "4 Queijos", preco: 5 }
+      { tipo: "4 Queijos", preco: 5, quantidade: 0 }
     ];
 
     $scope.pedidos = [];
 
-    var quantidadesRecheio = []
+    var arrQtdeRecheio = [];
+    var quantidadesRecheio = {};
 
-    $scope.mostraQuantidades = function (pedido) {
-        console.log(pedido.tipoPastel);
+    $scope.mostrarQuantidades = function (pedido) {
+        for (var i = 0; i < $scope.pedidos.length; i++) {
+            //console.log($scope.pedidos[i].tipoPastel.tipo);
+            //console.log(pedido.tipoPastel.tipo);
+            if ($scope.pedidos[i].tipoPastel.tipo == pedido.tipoPastel.tipo) {
+                //console.log(arrQtdeRecheio);
+                console.log("PAssou aqui 1");
+                //console.log(arrQtdeRecheio[i]);
+
+                if (!(arrQtdeRecheio[i] == pedido.tipoPastel.tipo)) {
+                    quantidadesRecheio = { tipo: pedido.tipoPastel.tipo, qtde: 1 };
+                    arrQtdeRecheio.push(quantidadesRecheio);
+                    
+                    console.log(arrQtdeRecheio);
+                    console.log("PAssou aqui2");
+                }
+                var qtde = quantidadesRecheio
+            }
+            console.log("-------------");
+            console.log(arrQtdeRecheio);
+        }
+        //$scope.pedidos
+        //console.log(quantidadesRecheio);
+        //console.log(quantidadesRecheio[i]);
     };
 
     $scope.adicionarPedido = function (pedido) {
         $scope.pedidos.push(angular.copy(pedido));
         delete $scope.pedido;
         $scope.pedidoForm.$setPristine();
-        $scope.mostraQuantidades(pedido);
+        $scope.mostrarQuantidades(pedido);
     };
 
     $scope.apagarPedido = function (pedidos) {
